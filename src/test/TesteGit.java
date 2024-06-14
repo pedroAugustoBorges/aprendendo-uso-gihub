aa54a26a81253d194b429fbcaf9fb036459b1145
Pedro <pedroameloborges@gmail.coom>
2024-06-14 18:35:06 -0300

    Aprendendo ausar commit

---
+++ src/test/TesteGit.java
@@ -0,0 +1,9 @@
+package test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TesteGit {
+    public static void main(String[] args){
+        System.out.println("Teste para git");
+    }
+}
