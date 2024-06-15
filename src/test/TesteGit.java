86201f3d972452117941531c07bb15cd54817b92
Pedro Augusto <pedroameloborges@gmail.com>
2024-06-15 14:12:11 -0300

    Inserindo atributos

--- src/test/TesteGit.java
+++ src/test/TesteGit.java
@@ -8,6 +8,12 @@ public class TesteGit {
     public static void main(String[] args){
         Aluno aluno = new Aluno("39340986814", 18, "Pedro Augusto") ;
 
+        int x = 10;
+
+        if (x < aluno.getIdade()){
+            System.out.println("Menor");
+        }
+
         System.out.println(aluno);
 
 
